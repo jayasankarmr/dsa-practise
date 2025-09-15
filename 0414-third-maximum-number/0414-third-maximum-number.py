731825574487885520674135d0dc859e0e6c70fe
Jayasankar M R <jayasankar.mr7@gmail.com>
2025-09-16 00:10:35 +0530

    Time: 0 ms (100%), Space: 13.1 MB (72.66%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        hashset = set(nums)
+        if len(hashset) < 3:
+            return max(hashset)
+        hashset.remove(max(hashset))
+        hashset.remove(max(hashset))
+        return max(hashset)
+        
