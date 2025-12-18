260f4c56aee524aa01a48ab209171ed6d8d8540d
Jayasankar M R <jayasankar.mr7@gmail.com>
2025-12-18 20:32:09 +0530

    Time: 31 ms (31.6%), Space: 18.4 MB (7.12%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+class Solution(object):
+    def groupAnagrams(self, strs):
+        anagrams_dict = defaultdict(list)
+        
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            
+            key = tuple(count)
+            anagrams_dict[key].append(s)
+            
+        return anagrams_dict.values()
+        
